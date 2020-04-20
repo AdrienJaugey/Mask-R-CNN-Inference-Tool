9607b7de8e3641ffb4a565461adaffe9e9490521
AdrienJaugey <a.jaugey@gmail.com>
2020-04-20 10:57:38 +0200

    Initial Commut

---
+++ CellsDataset.py
@@ -0,0 +1,100 @@
+import os
+import pathlib
+import numpy as np
+from cv2 import imread
+from skimage.transform import resize
+import utils, config
+
+
+class CellsDataset(utils.Dataset):
+    CLASS_NAMES = ["cortex", "tubule_sain", "tubule_atrophique", "nsg_complet", "nsg_partiel", "pac", "vaisseau"]
+
+    def __init__(self, train_ids, TRAIN_PATH):
+        super().__init__()
+        self.train_ids = train_ids
+        self.TRAIN_PATH = TRAIN_PATH
+
+    def load_cells(self, mode):
+        # Add classes
+        # self.add_class("cells", 1, "cortex")
+        # self.add_class("cells", 2, "tubule_sain")
+        self.add_class("cells", 3, "tubule_atrophique")
+        # self.add_class("cells", 4, "nsg_complet")
+        # self.add_class("cells", 5, "nsg_partiel")
+        # self.add_class("cells", 6, "pac")
+        # self.add_class("cells", 7, "vaisseau")
+
+        if mode == "train":
+            for n, id_ in enumerate(self.train_ids):
+                if n < int(len(self.train_ids) * 0.9):
+                    path = self.TRAIN_PATH + id_
+                    img_path = path + '/images/'
+                    self.add_image("cells", image_id=id_, path=img_path)
+
+        if mode == "val":
+            for n, id_ in enumerate(self.train_ids):
+                if n >= int(len(self.train_ids) * 0.9):
+                    path = self.TRAIN_PATH + id_
+                    img_path = path + '/images/'
+                    self.add_image("cells", image_id=id_, path=img_path)
+
+    def load_image(self, image_id):
+
+        info = self.image_info[image_id]
+        info = info.get("id")
+
+        path = self.TRAIN_PATH + info
+        img = imread(path + '/images/' + info + '.jp2')[:, :, :3]
+        img = resize(img, (config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1]), mode='constant', preserve_range=True)
+
+        return img
+
+    def image_reference(self, image_id):
+        """Return the cells data of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "cells":
+            return info["cells"]
+        else:
+            super(self.__class__).image_reference(self, image_id)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for cells of the given image ID.
+        """
+        TEST_MONOCLASS = True
+        info = self.image_info[image_id]
+        info = info.get("id")
+        path = self.TRAIN_PATH + info
+        # Counting masks for current image
+        number_of_masks = 0
+        for masks_dir in os.listdir(path):
+            # For each directory excepting /images
+            if TEST_MONOCLASS and masks_dir != 'tubule_atrophique':
+                continue
+            if masks_dir == 'images':
+                continue
+            temp_DIR = path + masks_dir
+            # Adding length of directory https://stackoverflow.com/questions/2632205/how-to-count-the-number-of-files-in-a-directory-using-python
+            number_of_masks += len(
+                [name for name in os.listdir(temp_DIR) if os.path.isfile(os.path.join(temp_DIR, name))])
+
+        mask = np.zeros([config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1], number_of_masks], dtype=np.uint8)
+        iterator = 0
+        class_ids = np.zeros((number_of_masks,), dtype=int)
+        for masks_dir in os.listdir(path):
+            temp_class_id = self.CLASS_NAMES.index(masks_dir) + 1
+            for mask_file in next(os.walk(path + '/' + masks_dir + '/'))[2]:
+                mask_ = imread(path + '/' + masks_dir + '/' + mask_file)
+                mask_ = resize(mask_, (config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1]), mode='constant',
+                               preserve_range=True)
+                mask[:, :, iterator] = mask_
+                class_ids[iterator] = temp_class_id
+                iterator += 1
+        # Handle occlusions
+        occlusion = np.logical_not(mask[:, :, -1]).astype(np.uint8)
+        for i in range(number_of_masks - 2, -1, -1):
+            mask[:, :, i] = mask[:, :, i] * occlusion
+            occlusion = np.logical_and(occlusion, np.logical_not(mask[:, :, i]))
+
+        # Map class names to class IDs.
+
+        return mask, class_ids.astype(np.int32)
---
+++ wrapper.py
@@ -0,0 +1,78 @@
+import os
+import xml.etree.ElementTree as ET
+import numpy as np
+import cv2
+
+
+def createMask(imgName, imgShape, noMask, ptsMask, datasetName='dataset_train', maskClass='masks'):
+    # Formatting the suffix for the image representing the mask
+    maskName = str('0' if noMask < 100 else '') + str('0' if noMask < 10 else '') + str(noMask)
+    # Defining path where the result image will be stored and creating dir if not exists
+    output_directory = 'dataset_train/' + imgName + '/' + maskClass + '/'
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # https://www.programcreek.com/python/example/89415/cv2.fillPoly
+    # Formatting coordinates matrix to get int
+    ptsMask = np.double(ptsMask)
+    ptsMask = np.matrix.round(ptsMask)
+    ptsMask = np.int32(ptsMask)
+
+    # Creating black matrix with same size than original image and then drawing the mask
+    mask = np.uint8(np.zeros((imgShape[0], imgShape[1])))
+    cv2.fillPoly(mask, [ptsMask], 255)
+
+    # Saving result image
+    output_name = imgName + maskName + '.png'
+    cv2.imwrite(output_directory + output_name, mask)
+
+
+def createMasksOfImage(directoryPath, imgName, datasetName='dataset_train'):
+    # Getting shape of original image (same for all this masks)
+    img = None
+    img = cv2.imread(directoryPath + '/' + imgName)
+    if img is None:
+        return
+    shape = img.shape
+
+    # Copying the original image in the dataset
+    fileName = imgName.split('.')[0]
+    targetDirectoryPath = datasetName + '/' + fileName + '/images/'
+    if not os.path.exists(targetDirectoryPath):
+        os.makedirs(targetDirectoryPath)
+        cv2.imwrite(targetDirectoryPath + imgName, img)
+
+    # https://www.datacamp.com/community/tutorials/python-xml-elementtree
+    tree = ET.parse(directoryPath + '/' + fileName + '.xml')
+    root = tree.getroot()
+    # Going through the XML tree and getting all Annotation nodes
+    for annotation in root.findall('./Annotations/Annotation'):
+        maskClass = annotation.attrib.get('PartOfGroup')
+        noMask = annotation.attrib.get('Name').replace('Annotation ', '')
+        ptsMask = []
+        # Going through the Annotation node and getting all Coordinate nodes
+        for points in annotation.find('Coordinates'):
+            xCoordinate = points.attrib.get('X')
+            yCoordinate = points.attrib.get('Y')
+            ptsMask.append([xCoordinate, yCoordinate])
+        # print('Mask ' + noMask + ': NbPts = ' + str(len(ptsMask)) + '\tclass = ' + maskClass)
+        createMask(fileName, shape, int(noMask), ptsMask, datasetName, maskClass)
+
+
+def startWrapper(rawDatasetPath)
+	# Getting all images, assuming that there are only images and xml annotation in directory
+	# mkyong.com/python/python-how-to-list-all-files-in-a-directory/
+	images = []
+	for _, _, files in os.walk(rawDatasetPath):
+		for file in files:
+			if '.xml' not in file:
+				images.append(file)
+
+	numberOfImages = len(images)
+	numberOfFiles = len(files)
+	if numberOfFiles - numberOfImages != numberOfImages:
+		print('It seems there are not as many images ({}) as other files ({})'.format(numberOfImages, numberOfFiles - numberOfImages))
+	for index in range(numberOfImages):
+		file = images[index]
+		print('Creating masks for {} image ({}/{})'.format(file, index + 1, numberOfImages))
+		createMasksOfImage(rawDatasetPath, file)
