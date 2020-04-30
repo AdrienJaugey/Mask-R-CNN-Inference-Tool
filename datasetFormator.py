ba05c01c464824b2f647c0a58f0a60db1e1118e2
AdrienJaugey <a.jaugey@gmail.com>
2020-04-30 12:20:28 +0200

    Added separation of cortex dataset and creation of the evaluation dataset using a random selection of the base dataset's images

--- datasetFormator.py
+++ datasetFormator.py
@@ -1,13 +1,17 @@
 import os
+from shutil import move
+import numpy as np
+
 import datasetWrapper as dW
 import datasetDivider as dD
 
 
-def infoNephrologyDataset(datasetPath: str):
+def infoNephrologyDataset(datasetPath: str, silent=False):
     """
     Print information about a dataset
     :param datasetPath: path to the dataset
-    :return: None
+    :param silent: if true nothing will be printed
+    :return: nbImg, histogram, cortexMissing, multiCortices, maxClasses, maxClassesNoCortex
     """
     print()
     histogram = {'tubule_atrophique': 0, 'vaisseau': 0, 'pac': 0, 'nsg_complet': 0,
@@ -17,6 +21,7 @@ def infoNephrologyDataset(datasetPath: str):
     maxNbClassesNoCortex = 0
     maxClassesNoCortex = []
     cortexMissing = []
+    printedCortexMissing = []
     multiCortices = []
     nbImg = 0
     for imageDir in os.listdir(datasetPath):
@@ -34,6 +39,9 @@ def infoNephrologyDataset(datasetPath: str):
                 histogram[maskDir] += 1
                 localHisto[maskDir] += 1
         if not cortex:
+            name = imageDir.split('_')[0] + "_*"
+            if name not in printedCortexMissing:
+                printedCortexMissing.append(name)
             cortexMissing.append(imageDir)
 
         if cortexDivided:
@@ -58,25 +66,81 @@ def infoNephrologyDataset(datasetPath: str):
                 maxClassesNoCortex = []
             maxClassesNoCortex.append(imageDir)
 
-    print("Nb Images : {}".format(nbImg))
-    print("Histogram : {}".format(histogram))
-    print("Missing cortices : {}".format(cortexMissing))
-    print("Multi cortices : {}".format(multiCortices))
-    print("Max Classes ({}) : {}".format(maxNbClasses, maxClasses))
-    print("Max Classes w/o cortex (({}) : {}".format(maxNbClassesNoCortex, maxClassesNoCortex))
+    if not silent:
+        print("{} dataset Informations :".format(datasetPath))
+        print("\tNb Images : {}".format(nbImg))
+        print("\tHistogram : {}".format(histogram))
+        print("\tMissing cortices ({}) : {}".format(len(cortexMissing), printedCortexMissing))
+        print("\tMulti cortices ({}) : {}".format(len(multiCortices), multiCortices))
+        print("\tMax Classes w/ cortex  ({}) :\t{}".format(maxNbClasses, maxClasses))
+        print("\tMax Classes w/o cortex ({}) :\t{}".format(maxNbClassesNoCortex, maxClassesNoCortex))
+    return nbImg, histogram, cortexMissing, multiCortices, maxClasses, maxClassesNoCortex
 
 
-# dW.startWrapper('raw_dataset', 'temp_nephrology_dataset', deleteBaseCortexMasks=False)
-# infoNephrologyDataset('temp_nephrology_dataset')
-# dD.divideDataset('temp_nephrology_dataset', 'nephrology_dataset', squareSideLength=1024)
-# infoNephrologyDataset('nephrology_dataset')
+def separateCortexDataset(datasetPath: str, cortexDatasetPath: str = None):
+    """
+    Move each image folder with no cortex present to the cortex dataset folder
+    :param datasetPath: path to the base dataset
+    :param cortexDatasetPath: path to the cortex dataset
+    :return: None
+    """
+    if cortexDatasetPath is None:
+        cortexDatasetPath = datasetPath + '_cortex'
+
+    _, _, toBeMoved, _, _, _ = infoNephrologyDataset(datasetPath, silent=True)
+    os.makedirs(cortexDatasetPath, exist_ok=True)
+    if len(toBeMoved) > 0:
+        print("Moving {} non-cortex images directories into correct dataset".format(len(toBeMoved)))
+        for imageWithoutCortexDir in toBeMoved:
+            move(os.path.join(datasetPath, imageWithoutCortexDir),
+                 os.path.join(cortexDatasetPath, imageWithoutCortexDir))
+
+
+def createEvalDataset(datasetPath: str, evalDatasetPath: str = None, evalDatasetSizePart=0.1, evalDatasetMinSize=30, rename=False, customRename: str = None):
+    """
+    Create the evaluation dataset by moving a random set of base dataset's images
+    :param datasetPath: path to the base dataset
+    :param evalDatasetPath: path to the eval dataset
+    :param evalDatasetSizePart: the part of the base dataset to be moved to the eval one
+    :param evalDatasetMinSize: the minimum size of the evaluation dataset
+    :param rename: whether or not you want to rename the base dataset after creation of the eval one
+    :param customRename: new name of the training dataset
+    :return: None
+    """
+    assert 0 < evalDatasetSizePart < 1
+    if evalDatasetPath is None:
+        evalDatasetPath = datasetPath + '_eval'
+
+    fullList = os.listdir(datasetPath)
+    evalDatasetSize = round(len(fullList) * evalDatasetSizePart)
+    if evalDatasetSize < evalDatasetMinSize:
+        evalDatasetSize = evalDatasetMinSize
+    assert len(fullList) > evalDatasetSize
+    toBeMoved = np.random.choice(fullList, evalDatasetSize, replace=False)
+
+    os.makedirs(evalDatasetPath, exist_ok=True)
+    if len(toBeMoved) > 0:
+        print("Moving {} images directories into eval dataset".format(len(toBeMoved)))
+        for dirName in toBeMoved:
+            move(os.path.join(datasetPath, dirName), os.path.join(evalDatasetPath, dirName))
+    if rename:
+        newName = (datasetPath + '_train') if customRename is None else customRename
+        move(datasetPath, newName)
+
+
+dW.startWrapper('raw_dataset', 'temp_nephrology_dataset', deleteBaseCortexMasks=False)
+infoNephrologyDataset('temp_nephrology_dataset')
+dD.divideDataset('temp_nephrology_dataset', 'nephrology_dataset', squareSideLength=1024)
+infoNephrologyDataset('nephrology_dataset')
 
 # If you want to keep all cortex files comment dW.cleanCortexDir() lines
 # If you want to check them and then delete them, comment these lines too and after checking use them
 # dW.cleanCortexDir('temp_nephrology_dataset')
 # dW.cleanCortexDir('nephrology_dataset')
 
-# dW.startWrapper('raw_dataset_test', 'temp_test_dataset', deleteBaseCortexMasks=False)
-# infoNephrologyDataset('temp_test_dataset')
-# dD.divideDataset('temp_test_dataset', 'test_dataset', squareSideLength=1024)
-# infoNephrologyDataset('test_dataset')
+
+separateCortexDataset('nephrology_dataset', 'nephrology_cortex_dataset')
+createEvalDataset('nephrology_dataset', rename=True)
+infoNephrologyDataset('nephrology_dataset_train')
+infoNephrologyDataset('nephrology_cortex_dataset')
+infoNephrologyDataset('nephrology_dataset_eval')
