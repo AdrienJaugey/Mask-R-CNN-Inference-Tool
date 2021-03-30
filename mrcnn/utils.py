Fixed int overflow with confusion matrix

mrcnn/utils.py
@@ -940,7 +940,7 @@ def compute_confusion_matrix(image_shape: iter, expectedResults: dict, predicted
     """
     expectedImg = create_multiclass_mask(image_shape, expectedResults, classes_hierarchy, config)
     predictedImg = create_multiclass_mask(image_shape, predictedResults, classes_hierarchy, config)
-    confusion_matrix = np.zeros((num_classes + 1, num_classes + 1), dtype=int)
+    confusion_matrix = np.zeros((num_classes + 1, num_classes + 1), dtype=np.int64)
     for y in range(image_shape[0]):
         for x in range(image_shape[1]):
             confusion_matrix[expectedImg[y, x]][predictedImg[y, x]] += 1
@@ -1032,7 +1032,7 @@ def compute_matches(gt_boxes, gt_class_ids, gt_masks, pred_boxes,
     """
     if nb_class > 0:
         bg = 1 if confusion_background_class else 0
-        confusion_matrix = np.zeros((nb_class + bg, nb_class + bg), dtype=np.int32)
+        confusion_matrix = np.zeros((nb_class + bg, nb_class + bg), dtype=np.int64)
     else:
         confusion_matrix = None
         confusion_iou_threshold = 1.

nephrology.py
@@ -127,7 +127,7 @@ class NephrologyInferenceModel:
         # Configurations
         nbClass = self.__NB_CLASS
         divSize = 1024 if self.__DIVISION_SIZE == "noDiv" else self.__DIVISION_SIZE
-        self.__CONFUSION_MATRIX = np.zeros((self.__NB_CLASS + 1, self.__NB_CLASS + 1), dtype=np.int32)
+        self.__CONFUSION_MATRIX = np.zeros((self.__NB_CLASS + 1, self.__NB_CLASS + 1), dtype=np.int64)
         self.__APs = []
 
         class SkinetConfig(Config):
@@ -653,7 +653,10 @@ class NephrologyInferenceModel:
                                                                              confusion_background_class=True,
                                                                              confusion_only_best_match=False)
                             if perPixelConfMatrix:
-                                hierarchy = [1, 2, {3: [4, 5]}, 6, 7, {8: [{9: 10}, 10]}]
+                                if self.__CORTEX_MODE:
+                                    hierarchy = [2, {3: 1}]
+                                else:
+                                    hierarchy = [1, 2, {3: [4, 5]}, 6, 7, {8: [{9: 10}, 10]}]
                                 confusion_matrix = utils.compute_confusion_matrix(
                                     image_shape=(imageInfo['HEIGHT'], imageInfo['WIDTH']), config=self.__CONFIG,
                                     expectedResults={'rois': gt_bbox, 'masks': gt_mask, 'class_ids': gt_class_id},